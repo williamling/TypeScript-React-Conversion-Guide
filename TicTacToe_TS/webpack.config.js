a4a8eb4311e19c2b4a6806b12ef538b93b6543ec
lingfuzhe <1569189962@qq.com>
2017-12-21 10:15:13 +0800

    [fix]

--- TicTacToe_TS/webpack.config.js
+++ TicTacToe_TS/webpack.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  // change to .tsx if necessary
   entry: './src/app.tsx',
   output: {
     filename: './dist/bundle.js'
   },
   resolve: {
-    // changed from extensions: [".js", ".jsx"]
     extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
   module: {
@@ -14,10 +12,5 @@ module.exports = {
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   },
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM",
-  },
-  // newline - add source-map support
   devtool: "source-map"
 }
